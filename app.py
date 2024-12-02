f61418f01f4131c9644fbd41708d275138f5783e
felixtoo48 <felixtoo48@gmail.com>
2024-12-03 02:00:23 +0300

    creating customer model

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ db = mysql.connector.connect(
 )
 
 
+# Models
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
+
+
 # creating routes to input/upload customers and orders
 @app.route('/customers', methods=['POST'])
 def add_customer():
