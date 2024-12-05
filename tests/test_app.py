8dcc8a258169016f6d060d01704d4f62e32ad080
felixtoo48 <felixtoo48@gmail.com>
2024-12-05 04:37:52 +0300

    importing pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,6 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import unittest
 import json
 from app import app, db, Customer, Order
+import pytest
 
 
 class TestAPI(unittest.TestCase):
